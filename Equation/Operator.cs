596d2c09cda205cc85d44ff51f0b2b10753af283
Vi Hanh Tran <73818705+vivzterr@users.noreply.github.com>
2020-11-03 19:19:55 +0100

    add Operator class

--- Equation/Operator.cs
+++ Equation/Operator.cs
@@ -8,5 +8,104 @@ namespace Equation
 {
 	class Operator
 	{
+		public static double sum (string op, string op2, int num1, int num2, int num3)
+		{
+			double total = 0;
+			if (op == "+")
+			{
+				if (op2 == "+")
+				{
+					total = num1 + num2 + num3;
+				}
+
+				else if (op2 == "-")
+				{
+					total = num1 + num2 - num3;
+				}
+
+				else if (op2 == "*")
+				{
+					total = num1 + num2 * num3;
+				}
+
+				else if (op2 == "/")
+				{
+					total = num1 + num2 / num3;
+				}
+			}
+
+			if (op == "-")
+			{
+				if (op2 == "+")
+				{
+					total = num1 - num2 + num3;
+				}
+
+				else if (op2 == "-")
+				{
+					total = num1 - num2 - num3;
+				}
+
+				else if (op2 == "*")
+				{
+					total = num1 - num2 * num3;
+				}
+
+				else if (op2 == "/")
+				{
+					total = num1 - num2 / num3;
+				}
+			}
+
+			if (op == "*")
+			{
+				if (op2 == "+")
+				{
+					total = num1 * num2 + num3;
+				}
+
+				else if (op2 == "-")
+				{
+					total = num1 * num2 - num3;
+				}
+
+				else if (op2 == "*")
+				{
+					total = num1 * num2 * num3;
+				}
+
+				else if (op2 == "/")
+				{
+					total = num1 * num2 / num3;
+				}
+			}
+
+			if (op == "/")
+			{
+				if (op2 == "+")
+				{
+					total = num1 / num2 + num3;
+				}
+
+				else if (op2 == "-")
+				{
+					total = num1 / num2 - num3;
+				}
+
+				else if (op2 == "*")
+				{
+					total = num1 / num2 * num3;
+				}
+
+				else if (op2 == "/")
+				{
+					total = num1 / num2 / num3;
+				}
+			}
+
+			return total;
+		}
+
+		
 	}
 }
